8bd491fa58b72534acc9caaaa95029fdb470fe4f
Abdelouahab <98978218+ADjoudi@users.noreply.github.com>
2024-01-25 14:12:24 +0100

    mod comment

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
+/* timestamp endpoint */
 router.get("/timestamp/:date", function (req, res, next) {
   const date = req.params.date ? new Date(req.params.date) : new Date();
   if (date == "Invalid Date") {
