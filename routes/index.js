c6387cd51d4f6f259353c6872a081cca7cfe261c
Abdelouahab <98978218+ADjoudi@users.noreply.github.com>
2024-02-03 12:11:44 +0100

    bug fixes

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,10 @@ var express = require("express");
 var router = express.Router();
 
 /* timestamp endpoint */
-router.get("/timestamp/api/:date", function (req, res, next) {
+router.get("/timestamp/api/:date?", function (req, res, next) {
+  if (/^\d+$/.test(req.params.date)) {
+    req.params.date = parseInt(req.params.date);
+  }
   const date = req.params.date ? new Date(req.params.date) : new Date();
   if (date == "Invalid Date") {
     res.status(400);
